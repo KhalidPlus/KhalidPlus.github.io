54d4d055e7452df9b613aa1d04815813a230e7d0
KhalidPlus <91458498+KhalidPlus@users.noreply.github.com>
2022-12-13 21:29:26 +0100

    Create script.js

---
+++ spanish/script.js
@@ -0,0 +1,140 @@
+const playerInstance = jwplayer("player").setup({
+  controls: true,
+  sharing: true,
+  displaytitle: true,
+  displaydescription: true,
+  abouttext: "Buy Me a Coffee",
+  aboutlink: "https://ko-fi.com/mekmekmek",
+
+
+  captions: {
+    color: "#FFF",
+    fontSize: 14,
+    backgroundOpacity: 0,
+    edgeStyle: "raised"
+  },
+
+  logo: {
+   file:
+      "https://upload.wikimedia.org/wikipedia/commons/8/89/HD_transparent_picture.png",
+  
+    link: "https://ko-fi.com/mekmekmek/"
+  },
+
+  captions: {
+    color: "#FFF",
+    fontSize: 14,
+    backgroundOpacity: 0,
+    edgeStyle: "raised"
+  },
+
+
+  playlist: [
+    {
+      title: "Mundial-Qatar.com",
+      description: "",
+      image: "https://estaticos-cdn.sport.es/clip/cc1f1efe-5198-459e-8e27-9d07d852a94b_alta-libre-aspect-ratio_default_0.jpg",
+      sources: [
+        {
+          file:
+            "https://stream.dlivecdn.com/dlive/transcode-65-213/suco5/1670954925/src/live.m3u8",
+          label: "1080p",
+          default: true
+        }
+      ],
+      captions: [
+        {
+          file:
+            "",
+          label: "English",
+          kind: "captions"
+        }
+      ],
+      tracks: [
+        {
+          file: "",
+          kind: "thumbnails"
+        }
+      ]
+    }
+  ],
+  advertising: {
+    client: "vast",
+    schedule: [
+      {
+        offset: "pre",
+        tag:
+          "https://www.videosprofitnetwork.com/watch.xml?key=b894254dd9d4fa8e241bfc02117a80d3"
+      }
+    ]
+  }
+});
+
+playerInstance.on("ready", function () {
+  // Call the player's `addButton` API method to add the custom button
+  playerInstance.addButton(iconPath, tooltipText, buttonClickAction, buttonId);
+
+  // This function is executed when the button is clicked
+  function buttonClickAction() {
+    const playlistItem = playerInstance.getPlaylistItem();
+    const anchor = document.createElement("a");
+    const fileUrl = playlistItem.file;
+    anchor.setAttribute("href", fileUrl);
+    const downloadName = playlistItem.file.split("/").pop();
+    anchor.setAttribute("download", downloadName);
+    anchor.style.display = "none";
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+  }
+
+  // Move the timeslider in-line with other controls
+  const playerContainer = playerInstance.getContainer();
+  const buttonContainer = playerContainer.querySelector(".jw-button-container");
+  const spacer = buttonContainer.querySelector(".jw-spacer");
+  const timeSlider = playerContainer.querySelector(".jw-slider-time");
+  buttonContainer.replaceChild(timeSlider, spacer);
+
+  // Detect adblock
+  playerInstance.on("adBlock", () => {
+    const modal = document.querySelector("div.modal");
+    modal.style.display = "flex";
+
+    document
+      .getElementById("close")
+      .addEventListener("click", () => location.reload());
+  });
+
+  // Forward 10 seconds
+  const rewindContainer = playerContainer.querySelector(
+    ".jw-display-icon-rewind"
+  );
+  const forwardContainer = rewindContainer.cloneNode(true);
+  const forwardDisplayButton = forwardContainer.querySelector(
+    ".jw-icon-rewind"
+  );
+  forwardDisplayButton.style.transform = "scaleX(-1)";
+  forwardDisplayButton.ariaLabel = "Forward 10 Seconds";
+  const nextContainer = playerContainer.querySelector(".jw-display-icon-next");
+  nextContainer.parentNode.insertBefore(forwardContainer, nextContainer);
+
+  // control bar icon
+  playerContainer.querySelector(".jw-display-icon-next").style.display = "none"; // hide next button
+  const rewindControlBarButton = buttonContainer.querySelector(
+    ".jw-icon-rewind"
+  );
+  const forwardControlBarButton = rewindControlBarButton.cloneNode(true);
+  forwardControlBarButton.style.transform = "scaleX(-1)";
+  forwardControlBarButton.ariaLabel = "Forward 10 Seconds";
+  rewindControlBarButton.parentNode.insertBefore(
+    forwardControlBarButton,
+    rewindControlBarButton.nextElementSibling
+  );
+
+  // add onclick handlers
+  [forwardDisplayButton, forwardControlBarButton].forEach((button) => {
+    button.onclick = () => {
+      playerInstance.seek(playerInstance.getPosition() + 10);
+    };
+  });
+});
